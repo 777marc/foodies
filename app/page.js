0ef7f6bdd1dadf57d6df5a63626b3557fe5c6b5a
Marc Mendoza <777marc@gmail.com>
2024-11-11 10:37:58 -0500

    add main page content

--- app/page.js
+++ app/page.js
@@ -1,17 +1,49 @@
 import Link from "next/link";
+import styleClassses from "../components/page.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Time to get started!
-      </h1>
-      <p>
-        <Link href="/meals">Explore Meals</Link>
-      </p>
-      <p>
-        <Link href="/community">Join the Community</Link>
-      </p>
-    </main>
+    <>
+      <header className={styleClassses.header}>
+        <div className={styleClassses.slideshow}></div>
+        <div>
+          <div className={styleClassses.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world</p>
+          </div>
+          <div className={styleClassses.cta}>
+            <Link href="/community">Join the community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={styleClassses.section}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={styleClassses.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
