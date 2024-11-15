d6937eac8e51a03a11c0019f5939c82e8b9b7f2d
Marc Mendoza <777marc@gmail.com>
2024-11-15 11:09:40 -0500

    default pages

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function error() {
+  return (
+    <div className="error">
+      <h1>An Error Occured!</h1>
+      <p>please try again later</p>
+    </div>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,4 @@
+"use client";
+export default function NotFound() {
+  return <h1 className="not-found">Page Not Found</h1>;
+}
