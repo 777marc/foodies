73fa3b13d25ccc7f7efd5788e7004fdecca16d4b
Marc Mendoza <777marc@gmail.com>
2024-11-15 09:50:26 -0500

    css clean up

--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -2,14 +2,18 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import styleClasses from "./main-header.module.css";
+import styleClasses from "./nav-link.module.css";
 
 export default function NavLink({ href, children }) {
   const path = usePathname();
   return (
     <Link
       href={href}
-      className={path.startsWith(href) ? styleClasses.active : undefined}
+      className={
+        path.startsWith(href)
+          ? `${styleClasses.active} ${styleClasses.link}`
+          : `${styleClasses.link}`
+      }
     >
       {children}
     </Link>
