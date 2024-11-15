5fc475376da65c57d6715e8c2a61b33d1e7bccdd
Marc Mendoza <777marc@gmail.com>
2024-11-15 11:34:57 -0500

    load details

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,5 +1,20 @@
+import { Suspense } from "react";
+import MealItem from "@/components/meals/meal-item";
+import { getMeal } from "@/db/meals";
+
+async function Meal({ slug }) {
+  const meal = await getMeal(slug);
+  return <MealItem {...meal} />;
+}
+
 function page({ params }) {
-  return <div>meal id: {params.slug}</div>;
+  return (
+    <main>
+      <Suspense fallback={<p className="loading">Loading...</p>}>
+        <Meal slug={params.slug} />
+      </Suspense>
+    </main>
+  );
 }
 
 export default page;
