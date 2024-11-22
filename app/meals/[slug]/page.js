a1704f41a0bbcb746f3b9162c565b0d488e9172a
Marc Mendoza <777marc@gmail.com>
2024-11-22 13:39:56 -0500

    finish

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -3,6 +3,19 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 import styleClasses from "./page.module.css";
 
+export async function generateMetadata({ params }) {
+  const meal = await getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default async function MealDetailsPage({ params }) {
   const meal = await getMeal(params.slug);
 
